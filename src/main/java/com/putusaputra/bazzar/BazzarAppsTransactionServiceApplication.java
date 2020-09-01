89eb683747d2ba62a4d8016ac5ae591f62774670
putusaputra <iputusaputra@gmail.com>
2020-09-01 14:49:59 +0800

    Initial commit

---
+++ src/main/java/com/putusaputra/bazzar/BazzarAppsTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.putusaputra.bazzar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazzarAppsTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazzarAppsTransactionServiceApplication.class, args);
+	}
+
+}
